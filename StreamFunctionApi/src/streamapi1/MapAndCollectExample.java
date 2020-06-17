ec51d56277a5c58243fe2bfaa6461788e12c6efe
HS_Dev <HS_Dev@192.168.1.6>
2020-06-17 22:17:22 +0530

    changes

--- StreamFunctionApi/src/streamapi1/MapAndCollectExample.java
+++ StreamFunctionApi/src/streamapi1/MapAndCollectExample.java
@@ -1,10 +1,13 @@
 package streamapi1;
 
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class MapAndCollectExample {
 	public static void main(String[] args) {
-		
+		List<String> nameList=Arrays.asList("sagar","ram");
 		User user=new User();
 		user.setAge(10);
 		user.setName("Sagar");
@@ -12,11 +15,18 @@ public class MapAndCollectExample {
 		User user1=new User();
 		user1.setAge(20);
 		user1.setName("Bhargav");
-		List<User> userList=new ArrayList();
+		List<User> userList=new ArrayList<User>();
 		userList.add(user1);
 		userList.add(user);
+		List<String> nameLeft=new ArrayList<String>();
+		userList.stream().filter(a-> a.getAge()<=30).map(User::getName)
+	    .collect(Collectors.toList());
+		;
 		
-		userList.stream().filter(a-> a.getAge()<=30).forEach(a->System.out.println(a.getAge()+"  "+a.getName()));
+		
+//	List<User> a=nameList.stream().filter(name->!(name.length()>4))
+//			 .map(s->) .collect(Collectors.toList());
+		
 	}
 
 }
