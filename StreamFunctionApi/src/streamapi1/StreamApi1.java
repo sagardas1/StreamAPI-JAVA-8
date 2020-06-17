bb71cb84b5bf7c655a49ab62476c18ce5c2b05a0
HS_Dev <HS_Dev@192.168.1.6>
2020-06-17 20:38:33 +0530

    1st stream commit, whats really happens

--- StreamFunctionApi/src/streamapi1/StreamApi1.java
+++ StreamFunctionApi/src/streamapi1/StreamApi1.java
@@ -1,5 +1,37 @@
 package streamapi1;
 
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class StreamApi1 {
+	
+	public static void main(String[] args) {
+		List<String> lists=Arrays.asList("sagar","bhargav","ravi");
+		//foreachMethod
+		lists.forEach(a->System.out.println(a));
+		System.out.println("-----------------------------------------");
+		
+	//	for filter() class use is Predicate<>()
+		lists.stream()
+			 .filter(new Predicate<String>() {
+
+				@Override
+				public boolean test(String name) {
+				
+					return !name.equals("sagar");
+				}
+			})
+			 //for foreach()  class used Consumer
+			 .forEach(new Consumer<String>() {
+
+				@Override
+				public void accept(String name) {
+					System.out.println(name);
+					
+				}
+			});
+		
+	}
 
 }
